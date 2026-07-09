e90f862ec981c4ead97db6a6f7974d5c9e1c366f
macmini <kesixin@hdedu.com>
2026-07-09 17:09:42 +0800

    更新代码

--- app.js
+++ app.js
@@ -1,51 +1,38 @@
-//app.js
-const Bmob = require('utils/Bmob-1.6.7.min.js');
-const utils = require('utils/util.js');
-//初始化
-Bmob.initialize("xx","xx");
-wx.u = utils
+// app.js
+var Api = require('utils/api.js');
 App({
-  onLaunch: function () {
-    Bmob.User.auth().then(res=>{
-      console.log(res)
-      console.log('一键登陆成功')
-      wx.setStorageSync('userId',res.objectId)
-    }).catch(err => {
-      console.log(err)
-    });
-    // // 展示本地存储能力
-    // var logs = wx.getStorageSync('logs') || []
-    // logs.unshift(Date.now())
-    // wx.setStorageSync('logs', logs)
-
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
-        }
-      }
-    })
-  },
-  globalData: {
-    userInfo: null
-  }
+    onLaunch() {
+        this.initApi();
+        wx.Api = Api;
+    },
+    globalData: {
+        userInfo: null
+    },
+    //初始化接口请求
+    initApi() {
+        var caller = function (url, oper, params, callback) {
+            var header = {
+                'Content-Type': 'application/json'
+            };
+            wx.request({
+                url,
+                method: oper,
+                data: params,
+                header,
+                success: function (res) {
+                    try {
+                        wx.stopPullDownRefresh();
+                    } catch (e) {}
+                    callback(res.statusCode == 200 ? null : res, res.data);
+                },
+                fail: function (err) {
+                    try {
+                        wx.stopPullDownRefresh();
+                    } catch (e) {}
+                    callback(err);
+                }
+            });
+        };
+        Api.init(caller);
+    }
 })
